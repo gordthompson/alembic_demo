caa9ba8b663933c0221c8d6b9c0e75c8632c406d
Gord Thompson <gord@gordthompson.com>
2022-06-11 08:29:53 -0600

    Remove index on invoice.account_number
    On some backends (e.g., MySQL) it causes
    a downgrade to fail because Alembic tries to
    drop the index first, but an existing FK
    relationship prevents it.

--- models/invoice.py
+++ models/invoice.py
@@ -7,7 +7,7 @@ from .__base import Base
 class Invoice(Base):
     __tablename__ = "invoice"
     invoice_number = Column(Integer, primary_key=True)
-    account_number = Column(Integer, ForeignKey("account.account_number"), index=True)
+    account_number = Column(Integer, ForeignKey("account.account_number"))
     invoice_date = Column(Date)
     invoice_amount = Column(Numeric(18, 4))
     account = relationship("Account", back_populates="invoices")
